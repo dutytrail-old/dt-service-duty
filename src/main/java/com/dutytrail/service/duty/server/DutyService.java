195aa68dc95926d35a593ac7d651d7a0eddc100a
joan.tolos <joan.tolos@thomsonreuters.com>
2016-06-11 09:28:55 +0200

    Trail service working

--- src/main/java/com/dutytrail/service/duty/server/DutyService.java
+++ src/main/java/com/dutytrail/service/duty/server/DutyService.java
@@ -1,12 +1,6 @@
 package com.dutytrail.service.duty.server;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
